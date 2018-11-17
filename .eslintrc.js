7a474423b4246fe4725e0a1c53ad4a474704871f
north <blarvinius@gmail.com>
2018-11-17 19:25:49 +0000

    reconcile deps, mostly linter plugins, add linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,13 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": [
+        "airbnb",
+        "react-app"
+    ],
+
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "jsx-a11y/href-no-hash": 0,
+    }
+
+
 };

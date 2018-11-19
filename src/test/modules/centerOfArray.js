38531f833ff6dd4754badc87c3dba09f3e7ce1eb
north <blarvinius@gmail.com>
2018-11-19 14:00:30 +0000

    setup to unit tesst/TDD algorithm ...GREEN

---
+++ src/test/modules/centerOfArray.js
@@ -0,0 +1,5 @@
+const value = true;
+
+module.exports = {
+  value
+};
---
+++ src/test/unit/center_test.js
@@ -0,0 +1,8 @@
+const expect = require("chai").expect;
+const center = require("../modules/centerOfArray");
+
+describe("A module algorithm", function() {
+  it("Will have a value", function() {
+    expect(center.value).to.be.true;
+  });
+});

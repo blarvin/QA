f4e6774aaa4f0146775111fee0cfaf991e69ade1
north <blarvinius@gmail.com>
2018-11-19 14:05:22 +0000

    cleanup eslint rules for testing

--- src/test/.eslintrc.js
+++ src/test/.eslintrc.js
@@ -1,21 +1,16 @@
 module.exports = {
-  "plugins": [
-    "cypress",
-    "chai-friendly"
-  ],
-  "env": {
-        "cypress/globals": true
+  plugins: ["cypress", "chai-friendly"],
+  env: {
+    "cypress/globals": true
   },
-  "rules": {
-        "no-unused-expressions": 0,
-        "chai-friendly/no-unused-expressions": 2,
-        "prefer-arrow-callback": 0,
-        "semi": 0,
-        "func-names": 0
+  rules: {
+    "no-unused-expressions": 0,
+    "chai-friendly/no-unused-expressions": 2,
+    "prefer-arrow-callback": 0,
+    "import/no-extraneous-dependencies": 0,
+    semi: 0,
+    "func-names": 0,
+    "prefer-destructuring": 0
   },
-  "extends": [
-    "plugin:cypress/recommended",
-    "prettier",
-    "prettier/react"
-  ]
-}
+  extends: ["plugin:cypress/recommended", "prettier", "prettier/react"]
+};

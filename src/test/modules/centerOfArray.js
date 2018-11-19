c04818107fd36bee1029b3050398f1083675ad99
north <blarvinius@gmail.com>
2018-11-19 17:42:10 +0000

    algorithm basically works

--- src/test/modules/centerOfArray.js
+++ src/test/modules/centerOfArray.js
@@ -1,5 +1,11 @@
 const centerOfArray = function(arr) {
-  return 3;
+  let theAnswer = null;
+  for (let i = 1; i < arr.length - 1; i += 1) {
+    if (leftSum(arr, i) === rightSum(arr, i)) {
+      theAnswer = i;
+    }
+  }
+  return theAnswer;
 };
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
@@ -12,7 +18,16 @@ const leftSum = function(arry, index) {
   return result.reduce(reducer);
 };
 
+const rightSum = function(arry, index) {
+  const result = [];
+  for (let i = index + 1; i < arry.length; i += 1) {
+    result.push(arry[i]);
+  }
+  return result.reduce(reducer);
+};
+
 module.exports = {
   centerOfArray,
-  leftSum
+  leftSum,
+  rightSum
 };
--- src/test/unit/center_test.js
+++ src/test/unit/center_test.js
@@ -4,13 +4,19 @@ const center = require("../modules/centerOfArray");
 const arr1 = [1, 1, 2, 7, 4];
 const arr2 = [133, 60, 23, 92, 6, 7, 168, 16, 19];
 const arr3 = [30, 43, 29, 10, 50, 40, 99, 51, 12];
+const arr4 = [1, 2, 3, 1, 9, 7];
 
 describe("An algorithm module", function() {
-  it("Will return 3", function() {
+  it("Will find the center", function() {
     expect(center.centerOfArray(arr1)).to.equal(3);
+    expect(center.centerOfArray(arr4)).to.equal(4);
   });
 
-  it("Will sum the values to left of a given index", function() {
+  it("Will sum the values to LEFT of a given index", function() {
     expect(center.leftSum(arr1, 3)).to.equal(4);
+  });
+
+  it("Will sum the values to RIGHT of a given index", function() {
+    expect(center.rightSum(arr1, 3)).to.equal(4);
   });
 });

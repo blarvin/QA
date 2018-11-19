91b6039e82ba4586a4706a567e72835edeb56886
north <blarvinius@gmail.com>
2018-11-19 16:01:45 +0000

    partial algorithm

--- src/test/modules/centerOfArray.js
+++ src/test/modules/centerOfArray.js
@@ -1,5 +1,18 @@
-const value = true;
+const centerOfArray = function(arr) {
+  return 3;
+};
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const leftSum = function(arry, index) {
+  const result = [];
+  for (let i = 0; i < index; i += 1) {
+    result.push(arry[i]);
+  }
+  return result.reduce(reducer);
+};
 
 module.exports = {
-  value
+  centerOfArray,
+  leftSum
 };
--- src/test/unit/center_test.js
+++ src/test/unit/center_test.js
@@ -1,8 +1,16 @@
 const expect = require("chai").expect;
 const center = require("../modules/centerOfArray");
 
-describe("A module algorithm", function() {
-  it("Will have a value", function() {
-    expect(center.value).to.be.true;
+const arr1 = [1, 1, 2, 7, 4];
+const arr2 = [133, 60, 23, 92, 6, 7, 168, 16, 19];
+const arr3 = [30, 43, 29, 10, 50, 40, 99, 51, 12];
+
+describe("An algorithm module", function() {
+  it("Will return 3", function() {
+    expect(center.centerOfArray(arr1)).to.equal(3);
+  });
+
+  it("Will sum the values to left of a given index", function() {
+    expect(center.leftSum(arr1, 3)).to.equal(4);
   });
 });
